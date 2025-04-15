026cc162e69c431a69c559bcadcec9fb6a072dc0
KISHOR RATHOD <kishorrathod@worthitcs.com>
2025-04-15 15:52:39 +0530

    committing

--- src/Basic/Basic.java
+++ src/Basic/Basic.java
@@ -1,3 +1,5 @@
+package Basic;
+
 public class Basic {
     public static void main(String[] args) {
         System.out.println("hello");
---
+++ src/Basic/Hello.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Basic.Hello People");
+//        Write a program to to print your name
+        System.out.println("Basic.Hello People this is Kishor Rathod");
+
+    }
+}
